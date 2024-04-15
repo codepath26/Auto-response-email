61e6a6dcf15419d03117ccddad9d12d721e3d4a4
parth thakor <parththakor2610@gmail.com>
2024-04-16 00:59:59 +0530

    auto generatre response mail

---
+++ middleware/passport.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const passport = require("passport");
+const { User } = require("../models/userModel");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+console.log(process.env.GOOGLE_CLIENT_SECRET);
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("Google Profile:", profile);
+      profile.tokens = { accessToken, refreshToken };
+      try {
+        const user = User.create({
+          googleId: profile.id,
+          displayName: profile.displayName,
+          email: profile.emails.value,
+          refreshToken: refreshToken,
+          accessToken: accessToken,
+        });
+        console.log(user, "user");
+      } catch (err) {
+        throw err;
+      }
+      return done(null, profile);
+    }
+  )
+);
+
+module.exports = passport;
